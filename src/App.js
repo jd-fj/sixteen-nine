6baa20a3390c0297283e6b1f203a04fd708c5343
TayLulz <taylulzcode@gmail.com>
2021-06-23 18:15:32 -0700

    import 16:9 image as video placeholder

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import ReactPlayer from 'react-player'
+import sixteenNine from './assets/img/16-9.png'
 
 
 function App() {
   return (
     <>
       hi
-      <img src="./assets/16-9.png"/>
+      <img src="sixteenNine"/>
     </>
   );
 }
