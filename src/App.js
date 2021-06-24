f00e5667abf37a66db0357b350bacafffa6e313e
TayLulz <taylulzcode@gmail.com>
2021-06-23 18:22:44 -0700

    test image

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import ReactPlayer from 'react-player'
-import sixteenNine from './assets/img/16-9.png'
+// import sixteenNine from './assets/img/16-9.png'
+import sixteenNine from './logo.svg'
 
 
 function App() {
   return (
     <>
       hi
-      <img src="sixteenNine"/>
+      <img src='sixteenNine' alt='16:9 aspect ratio'/>
     </>
   );
 }
